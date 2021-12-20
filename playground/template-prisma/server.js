3ce03f1198e788ee68f391bc908fc6cc5f7dc4e6
Jakob Niggel <jakobniggel@gmail.com>
2021-12-20 06:11:12 +0100

    fix: show error when start is execute before build

--- playground/template-prisma/server.js
+++ playground/template-prisma/server.js
@@ -5,5 +5,11 @@ require('tsconfig-paths').register({
     '~/*': ['./dist/*'],
   },
 });
+const fs = require('fs');
+
+if (!fs.existsSync('./dist/index.js')) {
+  console.error('Please run npm run build first.');
+  process.exit(1);
+}
 
 require('./dist/index');
