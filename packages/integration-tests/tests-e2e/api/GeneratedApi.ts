eac00a9dc072c1ee0c3f5f732af8057ce6bb4b8a
Jakob Niggel <jakobniggel@gmail.com>
2022-11-10 13:45:53 +0100

    feat: slim axios client

--- packages/integration-tests/tests-e2e/api/GeneratedApi.ts
+++ packages/integration-tests/tests-e2e/api/GeneratedApi.ts
@@ -139,10 +139,6 @@ export interface FullRequestParams extends Omit<AxiosRequestConfig, 'data' | 'pa
 export type RequestParams = Omit<FullRequestParams, 'body' | 'method' | 'query' | 'path'>;
 
 export interface ApiConfig<SecurityDataType = unknown> extends Omit<AxiosRequestConfig, 'data' | 'cancelToken'> {
-  securityWorker?: (
-    securityData: SecurityDataType | null,
-  ) => Promise<AxiosRequestConfig | void> | AxiosRequestConfig | void;
-  secure?: boolean;
   format?: ResponseType;
 }
 
@@ -154,51 +150,14 @@ export enum ContentType {
 
 export class HttpClient<SecurityDataType = unknown> {
   public instance: AxiosInstance;
-  private securityData: SecurityDataType | null = null;
-  private securityWorker?: ApiConfig<SecurityDataType>['securityWorker'];
-  private secure?: boolean;
   private format?: ResponseType;
 
-  constructor({ securityWorker, secure, format, ...axiosConfig }: ApiConfig<SecurityDataType> = {}) {
+  constructor({ format, ...axiosConfig }: ApiConfig<SecurityDataType> = {}) {
     this.instance = axios.create({ ...axiosConfig, baseURL: axiosConfig.baseURL || '' });
-    this.secure = secure;
     this.format = format;
-    this.securityWorker = securityWorker;
-  }
-
-  public setSecurityData = (data: SecurityDataType | null) => {
-    this.securityData = data;
-  };
-
-  private mergeRequestParams(params1: AxiosRequestConfig, params2?: AxiosRequestConfig): AxiosRequestConfig {
-    return {
-      ...this.instance.defaults,
-      ...params1,
-      ...(params2 || {}),
-      headers: {
-        ...(params1.headers || {}),
-        ...((params2 && params2.headers) || {}),
-      },
-    };
-  }
-
-  private createFormData(input: Record<string, unknown>): FormData {
-    return Object.keys(input || {}).reduce((formData, key) => {
-      const property = input[key];
-      formData.append(
-        key,
-        property instanceof Blob
-          ? property
-          : typeof property === 'object' && property !== null
-            ? JSON.stringify(property)
-            : `${property}`,
-      );
-      return formData;
-    }, new FormData());
   }
 
   public request = async <T = any, _E = any>({
-    secure,
     path,
     type,
     query,
@@ -206,17 +165,8 @@ export class HttpClient<SecurityDataType = unknown> {
     body,
     ...params
   }: FullRequestParams): Promise<T> => {
-    const secureParams =
-      ((typeof secure === 'boolean' ? secure : this.secure) &&
-        this.securityWorker &&
-        (await this.securityWorker(this.securityData))) ||
-      {};
-    const requestParams = this.mergeRequestParams(params, secureParams);
+    const requestParams = params;
     const responseFormat = (format && this.format) || void 0;
-
-    if (type === ContentType.FormData && body && body !== null && typeof body === 'object') {
-      body = this.createFormData(body as Record<string, unknown>);
-    }
 
     if (!type) {
       type = ContentType.Json;
